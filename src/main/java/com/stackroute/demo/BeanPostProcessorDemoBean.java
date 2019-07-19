8fd13d41d6f40a4da018478b4da42aa06608861a
Boddu Pranathi <37667750+pranathiboddu@users.noreply.github.com>
2019-07-19 09:43:27 +0530

    Update BeanPostProcessorDemoBean.java

--- src/main/java/com/stackroute/demo/BeanPostProcessorDemoBean.java
+++ src/main/java/com/stackroute/demo/BeanPostProcessorDemoBean.java
@@ -3,13 +3,16 @@ package com.stackroute.demo;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
-public class BeanPostProcessorDemoBean implements BeanPostProcessor {
+public class BeanPostProcessorDemoBean implements BeanPostProcessor //implemented beanpost processor
+{
 
-    public void initBean() {
+    public void initBean()  //initialized init
+    {
         System.out.println("Init Bean");
     }
     
-    public void destroyBean() {
+    public void destroyBean() //initialized destroy method
+    {
         System.out.println("Destroy bean");
     }
 
