657263b71b479116412818145c91171cf6207636
Boddu Pranathi <37667750+pranathiboddu@users.noreply.github.com>
2019-07-19 09:35:22 +0530

    Update Movie.java

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,17 +1,17 @@
 package com.stackroute.domain;
 
-public class Movie
+public class Movie 
 {
-    public Movie()
+    public Movie() //implemented default constructor
     {
 
     }
     Actor actor;
-    public void setActor(Actor actor)
+    public void setActor(Actor actor) 
     {
         this.actor = actor;
     }
-public void display()
+public void display() //calling actor method
 {
     actor.displayActorInfo();
 }
